f4d608da278dbad7905226ac25004d71be390ffb
nischalap <nischala.pandey@gmail.com>
2016-03-08 14:14:35 -0500

    CoursesAssignments

---
+++ Jan2016/Nish/com/java/courses/Courses.java
@@ -0,0 +1,54 @@
+package com.java.courses;
+
+import java.util.List;
+
+public class Courses {
+	private String courseName;
+	private String duration;
+	private String fees;
+	private List<Student> students;
+	private List<Trainer> trainer;
+	
+	
+	public Courses(String courseName, String duration, String fees, List<Student> students, List<Trainer> trainer) {
+		super();
+		this.courseName = courseName;
+		this.duration = duration;
+		this.fees = fees;
+		this.students = students;
+		this.trainer = trainer;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public String getFees() {
+		return fees;
+	}
+	public void setFees(String fees) {
+		this.fees = fees;
+	}
+	public List<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+	public List<Trainer> getTrainer() {
+		return trainer;
+	}
+	public void setTrainer(List<Trainer> trainer) {
+		this.trainer = trainer;
+	}
+   public void displayInfo(){
+	   System.out.println(courseName+"  \n"+trainer+"   ");
+   }
+}
---
+++ Jan2016/Nish/com/java/courses/Trainer.java
@@ -0,0 +1,37 @@
+package com.java.courses;
+
+public class Trainer {
+	private String trainerName;
+	private String trainerId;
+	private String coursesHandle;
+	public Trainer(String trainerName, String trainerId, String coursesHandle) {
+		super();
+		this.trainerName = trainerName;
+		this.trainerId = trainerId;
+		this.coursesHandle = coursesHandle;
+	}
+	public String getTrainerName() {
+		return trainerName;
+	}
+	public void setTrainerName(String trainerName) {
+		this.trainerName = trainerName;
+	}
+	public String getTrainerId() {
+		return trainerId;
+	}
+	public void setTrainerId(String trainerId) {
+		this.trainerId = trainerId;
+	}
+	public String getCoursesHandle() {
+		return coursesHandle;
+	}
+	public void setCoursesHandle(String coursesHandle) {
+		this.coursesHandle = coursesHandle;
+	}
+	@Override
+	public String toString() {
+		return "Trainer:  " + trainerName + ", ";
+				//+ "trainerId=" + trainerId + ", coursesHandle=" + coursesHandle
+				//+ "]";
+	}
+}
