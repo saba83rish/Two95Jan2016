dd6e55575a6394b70c9c01f949cfb4e27e48b5a8
HridyaGopu <hridyag23@gmail.com>
2016-03-07 20:01:23 -0600

    Added files via upload

---
+++ Ctest.java
@@ -0,0 +1,22 @@
+package com.two95.Card;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+import com.two95.Card.Cards;
+
+public class Ctest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BeanFactory factory=new XmlBeanFactory(new FileSystemResource("Card.xml"));
+		Cards s=(Cards) factory.getBean("s1");
+		s.display_clubs();
+		s.display_spade();
+		s.display_diamonds();
+		s.display_hearts();
+	}
+
+}
